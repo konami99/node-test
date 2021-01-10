38d55053c18d4c43638b9def4d73805d06d38763
Richard Chou <richard.chou@shippit.com>
2021-01-10 20:52:59 +1100

    define worker

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,10 +1,24 @@
 const express = require('express')
-const { queue } = require('../services/resque');
+const { Queue } = require('node-resque');
+
+const connectionDetails = {
+  pkg: "ioredis",
+  host: "127.0.0.1",
+  password: null,
+  port: 6379,
+  database: 0,
+  // namespace: 'resque',
+  // looping: true,
+  // options: {password: 'abc'},
+};
 
 const router = new express.Router()
 
+const queue = new Queue({ connection: connectionDetails });
+
 router.get('/users', async (req, res) => {
-  await queue.enqueue("math", "add", [1, 2])
+  await queue.connect();
+  await queue.enqueue("math", "add", [1, 2]);
 
   res.status(201).send([{
     id: 1,
